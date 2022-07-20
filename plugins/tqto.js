25858bae35fb215b40222b9f192efaa875b6aa85
elaina12Md <109229034+elaina12Md@users.noreply.github.com>
2022-07-20 13:47:36 +0800

    Update tqto.js

--- plugins/tqto.js
+++ plugins/tqto.js
@@ -7,30 +7,10 @@
 let handler = async (m, { conn, usedPrefix }) => {
     let tqto = `*BIG THANKS TO*
 
-Nurutomo: 
-https://github.com/Nurutomo
-Istikmal: 
-https://github.com/BochilGaming
-Ariffb: 
-https://github.com/Ariffb25
-Aguz Familia:
-https://github.com/FokusDotId
-Amelia Lisa:
-https://github.com/Ameliascrf
-Aniq12
-https://github.com/aniq12
-Ilman: 
-https://github.com/ilmanhdyt
-Amirul: 
-https://github.com/amiruldev20
-Irwan:
-https://github.com/irwanx
-Rasel:
-https://github.com/raselcomel
-Mursid:
-https://github.com/Botwa021
-Jarot:
-https://github.com/Alpiii22
+Amell 
+https://chat.whatsapp.com/J9v6iwwrDvX3zPpEnLbVwB
+Amellbotz
+https://youtube.com/channel/UC_KcMlX5LI9EZIVOgLOx9TQ
 
 \`\`\`${conn.user.name}\`\`\`
 `
