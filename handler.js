de0d3c5c79bcb09d43b7d6cad13c105afc6829ad
elaina12Md <109229034+elaina12Md@users.noreply.github.com>
2022-07-20 13:54:42 +0800

    Update handler.js

--- handler.js
+++ handler.js
@@ -30,14 +30,14 @@ module.exports = {
         global.namabot = conn.user.name
         global.packname = global.namabot
         global.author = global.data.owner
-        //global.author = '𝐉𝐚𝐫𝐨𝐭'
+        //global.author = 'Amell'
         global.wm2 = global.data.namabot + ' ' + global.data.owner
-        global.wm = '© 𝐀𝐥𝐩𝐢 𝐁𝐨𝐭'
+        global.wm = 'Hello Good Kami Di Sini Tidak Mau kalau anda Terkena Banned Kerna Masalah Spam Mohon Kurangi Tingkah Laku anda'
         global.colong1 = 'Ciss 📸'
-        global.colong2 = '𝐀𝐥𝐩𝐢 𝑩𝒐𝒕'
+        global.colong2 = 'Amell-Botz'
         global.kontak2 = [
-        ['6285850539404', '𝐉𝐚𝐫𝐨𝐭', '𝑫𝒆𝒗𝒆𝒍𝒐𝒑𝒆𝒓 𝑩𝒐𝒕', 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ', true],
-        ['6285713137903', await this.getName('6285713137903@s.whatsapp.net'), '𝑷𝒂𝒓𝒕𝒏𝒆𝒓 𝐀𝐥𝐩𝐢 𝑩𝒐𝒕', 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ', true], 
+        ['62895366943070', 'ᴀᴍᴇʟʟ', '𝑫𝒆𝒗𝒆𝒍𝒐𝒑𝒆𝒓 𝑩𝒐𝒕', 'https://youtube.com/channel/UC_KcMlX5LI9EZIVOgLOx9TQ', true],
+        ['62895366943070', await this.getName('6285713137903@s.whatsapp.net'), 'ᴘʀᴀɴᴛʜᴇʀ ᴀᴍᴇʟʟ', 'https://chat.whatsapp.com/J9v6iwwrDvX3zPpEnLbVwB', true], 
         ]
         global.bg = await (await fetch(img)).buffer()
         global.time = require('moment-timezone').tz('Asia/Jakarta').format('HH:mm:ss')
